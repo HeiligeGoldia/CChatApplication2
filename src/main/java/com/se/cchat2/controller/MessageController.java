468c212cb40edcc87bfe3db9f1a1df5f401aff30
Duy <heiligegoldia@gmail.com>
2022-10-21 21:03:23 +0700

    test 3

--- src/main/java/com/se/cchat2/controller/MessageController.java
+++ src/main/java/com/se/cchat2/controller/MessageController.java
@@ -5,6 +5,7 @@ import com.se.cchat2.repository.MessageRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -14,7 +15,7 @@ public class MessageController {
     private MessageRepository messageRepository;
 
     @MessageMapping("/chat/{cid}")
-    public String send(@DestinationVariable String cid, Message newMess){
+    public String send(@DestinationVariable String cid, @RequestBody Message newMess){
         return messageRepository.send(cid, newMess);
     }
 
